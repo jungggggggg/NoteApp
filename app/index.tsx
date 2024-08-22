a3d35fdfbcaa940f3298eaacb9f8b8c28ff2e0f0
jungggggggg <jungsu0705@naver.com>
2024-08-22 15:06:30 +0900

    vercel

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import { View, ActivityIndicator, StyleSheet } from 'react-native';
 import * as Font from 'expo-font';
 import MainPage from './MainPage';
 
 // 폰트 로드 함수
-const loadFonts = () => {
-  return Font.loadAsync({
+const loadFonts = async () => {
+  await Font.loadAsync({
     'GodoB': require('../assets/GodoB.ttf'),
     'GodoM': require('../assets/GodoB.ttf'),
     'godoMaum': require('../assets/godoMaum.ttf'),
@@ -17,10 +18,32 @@ const loadFonts = () => {
 };
 
 export default function LoadFont() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
 
-    useEffect(() => {
-        loadFonts()
-    },[])
+  useEffect(() => {
+    const loadAllFonts = async () => {
+      await loadFonts();
+      setFontsLoaded(true); // 폰트 로드 완료 시 상태 업데이트
+    };
+
+    loadAllFonts();
+  }, []);
+
+  if (!fontsLoaded) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="gray" />
+      </View>
+    );
+  }
 
   return <MainPage />; // 폰트 로드 후 앱 화면 렌더링
 }
+
+const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
