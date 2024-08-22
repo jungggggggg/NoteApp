5189b187359ad71c1b0127e004c1a089239e5245
jungggggggg <jungsu0705@naver.com>
2024-08-22 12:18:00 +0900

    changing expo-router

---
+++ app/index.tsx
@@ -0,0 +1,28 @@
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+
+
+export default function MainPage(){
+    return (
+        <SafeAreaView style={styles.container}>
+            
+            {/* 헤더 */}
+
+            {/* 네비게이션 */}
+
+            {/* 태그선택 */}
+
+            {/* 태그에 맞는 메모 */}
+
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    header: {
+        flexDirection: 'row',
+    },
+    
+})
---
+++ styles/colors.tsx
@@ -0,0 +1,9 @@
+
+export default {
+    fontColor: '#0B1430',
+    blue: '#7ECBFF',
+    orange: '#FFA448',
+    pink: '#FFA6C4',
+    greenBlue: '#1CCCC3',
+    strongPink: '#FFA3A3'
+}
